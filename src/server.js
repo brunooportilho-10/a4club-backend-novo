3715f61394c1696799ec84de8bba63ae3fc3704c
brunooportilho-10 <brunoo.portilho@gmail.com>
2026-07-18 14:00:33 -0300

    Delete src/server.js

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.json({ ok: true }));
-app.get('/health', (req, res) => res.json({ ok: true, message: 'Working' }));
-app.post('/auth/login', (req, res) => res.json({ token: 'test', user: { id: 1 } }));
-
-app.listen(3000);
